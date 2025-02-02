c75e8cb73fecb41b2d53cdc2bcfe55108417d6a0
Anjali Vibhute <101789772+VibhuteAnjali@users.noreply.github.com>
2025-02-02 14:10:32 +0530

    Update AboutUs.jsx

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -7,7 +7,7 @@ export default function AboutUs() {
             <p className='headline'>ABOUT US</p>
             <div className='flexContainer'>
                 <div className="infoAboutUs">
-                    <p>Founded in 2022 in Vadodara, Gujarat, RK Trading Co. is a trusted name in premium industrial materials. We provide top-quality products like Densified Laminated Wood, Glass Epoxy Sheets, Phenolic Laminate, and more. With a focus on quality, reliability, and customer satisfaction, we deliver tailored solutions and timely service to industries nationwide.
+                    <p>Founded in 2018 in Ekaxara Industries is a trusted name in premium industrial materials. We provide top-quality products like Densified Laminated Wood, Glass Epoxy Sheets, Phenolic Laminate, and more. With a focus on quality, reliability, and customer satisfaction, we deliver tailored solutions and timely service to industries nationwide.
                     </p>
                 </div>
                 <div className='AboutImg'>
