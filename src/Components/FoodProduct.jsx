47f75eaa2e43311c0c79b8af341110e9067288af
Anjali Vibhute <101789772+VibhuteAnjali@users.noreply.github.com>
2025-03-29 14:12:09 +0530

    Update FoodProduct.jsx

--- src/Components/FoodProduct.jsx
+++ src/Components/FoodProduct.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import "../style/ProductCard.css"
-export default function ProductCard({name,img}) {
+export default function FoodProduct({name,img}) {
   return (
     <div style={{ width: '18rem' }} border="dark">
     <img variant="top" src={img} className='cardImg' alt='productImg'/>
